82cae6eb462dff02c73fec3d2466805deeae2ee1
Subhas Bhat <bhatsubhas@gmail.com>
2019-04-16 17:28:54 +0530

    Add for loop which breaks at 6

--- welcome.c
+++ welcome.c
@@ -8,6 +8,16 @@ int main()
 	test_function();
 	int value = get_increment(99);
 	printf("Incremented value is %d\n", value);
+	int i;
+	for(i=0; i<10; i++)
+	{
+		printf("%d",i);
+		if(i == 6)
+		{
+			break;
+		}
+	}
+	printf("\n");
 }
 void test_function()
 {
